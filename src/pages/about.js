c0a6f24ccd594ac5d13c6e2543c4a1d38eb66362
Eric Murphy <hi@ericnmurphy.com>
2019-02-21 14:36:03 +0700

    Added about page

---
+++ src/pages/about.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import SEO from '../components/seo'
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <SEO title="About Us" />
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis,
+        nibh vel mattis eleifend, tellus augue volutpat urna, a euismod orci
+        dolor quis risus. Mauris vel massa ut ante accumsan vulputate eu
+        porttitor urna.
+      </p>
+
+      <p>
+        Proin hendrerit felis eu urna laoreet imperdiet. Sed volutpat lectus sed
+        magna tempor, id facilisis libero cursus. Pellentesque arcu lectus,
+        convallis et urna sed, dapibus gravida erat. Phasellus tristique eget ex
+        id sollicitudin. Donec lacinia rutrum consectetur. Aenean magna purus,
+        dictum et magna sit amet, tincidunt feugiat leo. Sed ultricies consequat
+        quam, nec consectetur velit molestie sed. Vivamus a blandit nunc, vel
+        bibendum ipsum.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
